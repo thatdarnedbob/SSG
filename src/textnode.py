d0ed49f994467ad59a274233c7305a0de62703f2
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-05-05 22:29:51 -0500

    text node to HTML node

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import HTMLNode, LeafNode, ParentNode
+
 
 class TextType(Enum):
-    NORMAL = "normal"
     TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
@@ -23,4 +24,20 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-        
+    
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, "href")
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt":text_node.text})
+        case _:
+            raise Exception("not a recognized text type")
