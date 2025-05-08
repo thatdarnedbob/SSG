11bdc255fa98971c34641ea18171d1af7da97f42
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-05-08 14:13:16 -0500

    more tests

--- src/test_processes.py
+++ src/test_processes.py
@@ -276,26 +276,6 @@ class TestProcesses(unittest.TestCase):
             ],
             new_nodes,
         )
-
-    def test_markdown_to_blocks(self):
-        md = """
-This is **bolded** paragraph
-
-This is another paragraph with _italic_ text and `code` here
-This is the same paragraph on a new line
-
-- This is a list
-- with items
-"""
-        blocks = markdown_to_blocks(md)
-        self.assertEqual(
-            blocks,
-            [
-                "This is **bolded** paragraph",
-                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
-                "- This is a list\n- with items",
-            ],
-        )
     
 if __name__ == "__main__":
     unittest.main()
