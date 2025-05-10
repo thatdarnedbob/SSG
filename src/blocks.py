ce0a61bc119a7a339a59f5b50cf90c2034acf134
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-05-09 22:36:20 -0500

    first failing tests for markdown blocks

---
+++ src/blocks.py
@@ -0,0 +1,29 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+def block_to_block_type(block):
+    pass
+
+def is_header_block(block):
+    pattern = re.compile("#{1,6} .*", flags=re.DOTALL)
+    return pattern.match(block)
+
+def is_code_block(block):
+    pass
+
+def is_quote_block(block):
+    pass
+
+def is_unordered_list_block(block):
+    pass
+
+def is_ordered_list_block(block):
+    pass
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,6 @@
 import unittest
 from processes import markdown_to_blocks
+from blocks import *
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -45,7 +46,36 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+    def test_markdown_to_blocks_aggressive_whites(self):
+        md = """
+        
 
+
+
+                This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line            
+
+
+
+
+
+            - with items                                   
+
+
+
+
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- with items",
+            ],
+        )
+
+    def test_is_header_block(self):
+        self.assertEqual(is_header_block("# hey you"), True)
 
 if __name__ == "__main__":
     unittest.main()
