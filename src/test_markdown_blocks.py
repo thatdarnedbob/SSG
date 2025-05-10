b4ae2abc9b244854d78da2034247b1c43c30f15b
thatdarnedbob <123yiaFTUTi121yiuhjk>
2025-05-09 22:57:59 -0500

    fuller testing for is heading block

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -75,11 +75,29 @@ This is the same paragraph on a new line
         )
 
     def test_is_header_block(self):
-        res = is_header_block("# hey you")
-        self.assertEqual(res, True)
+        nonce_str = "hey you"
+        nonce_str_multiline = "   boogedie\ndooooo"
+        good_nonced_headers = []
 
-        res = is_header_block(" # hey you")
-        self.assertEqual(res, False)
+        for i in range(1,7):
+            good_nonced_headers.append('#'*i + ' ' + nonce_str)
+        for header_block in good_nonced_headers:
+            self.assertEqual(is_header_block(header_block), True)
+
+        good_nonced_headers_multiline = []
+
+        for i in range(1,7):
+            good_nonced_headers_multiline.append('#'*i + ' ' + nonce_str_multiline)
+        for header_block in good_nonced_headers_multiline:
+            self.assertEqual(is_header_block(header_block), True)
+
+        bad_nonced_headers = []
+        for i in range(1,7):
+            bad_nonced_headers.append(' #'*i + ' ' + nonce_str)
+        for header_block in bad_nonced_headers:
+            self.assertEqual(is_header_block(header_block), False)
+
+        self.assertEqual(is_header_block('#'*7 + ' ' + nonce_str_multiline), False)
 
 
 if __name__ == "__main__":
